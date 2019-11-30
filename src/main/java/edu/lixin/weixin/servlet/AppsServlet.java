5885b012a607939ac1e9773da9b4c15b027c94ea
HuKaiLang <1186470720@qq.com>
2019-11-30 21:42:16 +0800

    update projects

--- src/main/java/edu/lixin/weixin/servlet/AppsServlet.java
+++ src/main/java/edu/lixin/weixin/servlet/AppsServlet.java
@@ -1,6 +1,10 @@
 package edu.lixin.weixin.servlet;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import edu.lixin.factory.BeanFactory;
+import edu.lixin.weixin.model.App;
+import edu.lixin.weixin.service.IAppsService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,11 +14,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
 @WebServlet(urlPatterns = "/weixin/getapps")
 public class AppsServlet extends HttpServlet {
+
+    private IAppsService service = BeanFactory.getInstance("appsService",IAppsService.class);
+
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("AppsServlet called");
@@ -23,6 +32,9 @@ public class AppsServlet extends HttpServlet {
         resp.setContentType("application/json;charset=utf-8");
         OutputStream outputStream = resp.getOutputStream();
 
+        List<App> list = service.query();
+        System.out.println(list.toString());
+
         Map<String,String> param1 = new HashMap<String,String>();
         param1.put("UserName","username");
         param1.put("PassWd","password");
@@ -30,8 +42,8 @@ public class AppsServlet extends HttpServlet {
         jsonObject.put("data",param1);
         System.out.println(jsonObject.toString());
         System.out.println(jsonObject.toJSONString());
-        outputStream.write(jsonObject.toString().getBytes());
-        outputStream.write(jsonObject.toJSONString().getBytes());
+
+        outputStream.write(JSON.toJSONBytes(list));
         outputStream.flush();
     }
 }
