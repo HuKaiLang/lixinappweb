dc1aa330f8963a69a12c386a492311b1d09729c1
HuKaiLang <1186470720@qq.com>
2019-12-13 13:56:31 +0800

    更新了UserServlet.java
    调整了获取参数的方法；添加了用于修改用户余额和电费的接口

--- src/main/java/edu/lixin/servlet/UserServlet.java
+++ src/main/java/edu/lixin/servlet/UserServlet.java
@@ -3,6 +3,7 @@ package edu.lixin.servlet;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import edu.lixin.factory.BeanFactory;
+import edu.lixin.utils.ParameterUtils;
 import edu.lixin.weixin.model.User;
 import edu.lixin.weixin.service.IUserService;
 
@@ -13,9 +14,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@WebServlet(urlPatterns = "/weixin/getUserInfo")
+@WebServlet(urlPatterns = "/weixin/user")
 public class UserServlet extends HttpServlet {
     private static IUserService service = BeanFactory.getInstance("userService", IUserService.class);
 
@@ -23,16 +26,44 @@ public class UserServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setCharacterEncoding("utf-8");
         resp.setContentType("application/json;charset=utf-8");
-        String user_school_id = req.getParameter("user_school_id");
+        ParameterUtils parameterUtils = new ParameterUtils(req);
         OutputStream outputStream = resp.getOutputStream();
 
-        List<User> list = service.query();
-        System.out.println(list.get(0).toString());
+        String method = parameterUtils.parseParameter("method");
+        String user_school_id = parameterUtils.parseParameter("user_school_id");
 
-        User user = service.findBySchoolId(Integer.parseInt(user_school_id));
-        System.out.println(JSON.toJSONString(user));
+//        if(req.getMethod().equals("POST")){
+//            Map<String, String> params = new HashMap<>();
+//            Map<String, String[]> parameterMap = req.getParameterMap();
+//            if (parameterMap != null) {
+//                for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+//                    params.put(entry.getKey(), entry.getValue()[0]);
+//                }
+//            }
+//            method = params.get("method");
+//        }else{
+//            method = req.getParameter("method");
+//        }
 
-        outputStream.write(JSON.toJSONBytes(list.get(0), SerializerFeature.WriteMapNullValue));
-        outputStream.flush();
+        if(method == null){
+            List<User> list = service.query();
+
+            User user = service.findBySchoolId(Integer.parseInt(user_school_id));
+
+            outputStream.write(JSON.toJSONBytes(list.get(0), SerializerFeature.WriteMapNullValue));
+            outputStream.flush();
+        }else{
+            String user_charge = parameterUtils.parseParameter("user_charge");
+            String user_balance = parameterUtils.parseParameter("user_balance");
+            int user_id = Integer.parseInt(parameterUtils.parseParameter("user_id"));
+            switch (method){
+                case "updateCharge":
+                    service.updateUserCharge(user_id,Float.parseFloat(user_charge));
+                    break;
+                case "updateBalance":
+                    service.updateUserBalance(user_id,Float.parseFloat(user_balance));
+                    break;
+            }
+        }
     }
 }
